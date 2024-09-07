5731b3a40485094eda81ea335bf0f1bfab34357c
ImoEazy <imieadams@gmail.com>
2024-09-07 16:13:15 +0200

    console user preferance

--- index.js
+++ index.js
@@ -5,15 +5,18 @@
 // print whether the user prefers a "Regular" or "Sugar-free" variety. If the preference is not set, default to "Regular".
 
 let userPreference = 'Sugar-free'; // Possible values: 'Regular', 'Sugar-free', or undefined
-let message = userPreference = sugarFree ? "regular" : "default";
-console.log(``);
+userPreference === 'Sugar-free' ? console.log(`hi ther`): console.log('User prefers: Regular')//sugar free would be the chosen object, regular is meant to be default and undefined = "not sugar free"
+
+console.log(`${currentHour < 24 && currentHour > 7 ? 'Unleash the beast!' : 'Better stick to water.'}`);
+
 
 // #### Challenge 2: Stock Check
 // You have a variable that tracks the number of Monster Energy cans left in the fridge. 
 // Use a ternary operator to print "Time to restock!" if the number is less than 5, or "We're stocked!" if the number is 5 or more.
 
 let cansLeft = 3; // Any number of cans
-console.log(``);
+let message = cansLeft >= 5? "Time to restock":"we're stocked!";
+console.log(`message`);
 
 
 // #### Challenge 3: Workout Intensity
